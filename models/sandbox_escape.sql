fe05702e1d62b821c7fadd84f263de050c960d85
hunter <hunter@WD168220234152noidaspeedvpsFY1KQ5t4wW.test.com>
2026-09-23 13:19:41 +0530

    sandbox escape v2 - no secret env ref

--- models/sandbox_escape.sql
+++ models/sandbox_escape.sql
@@ -1,84 +1,40 @@
 {# Probe re module globals for builtins access #}
 {%- set re_mod = modules.re -%}
-
-{# Method 1: re.compile type #}
 {{ log("RE_COMPILE_TYPE=" ~ re_mod.compile | string, info=True) }}
 
-{# Method 2: Try accessing __globals__ via re._compile or similar #}
-{%- if re_mod.compile.__globals__ is defined -%}
-  {{ log("RE_GLOBALS_AVAILABLE=YES", info=True) }}
-  {{ log("RE_GLOBALS_KEYS=" ~ re_mod.compile.__globals__.keys() | list | join(',') | truncate(500), info=True) }}
-{%- else -%}
-  {{ log("RE_GLOBALS_NOT_ACCESSIBLE", info=True) }}
-{%- endif -%}
+{# Method 1: Try __globals__ through attr filter #}
+{%- set gg = re_mod.compile | attr('__globals__') -%}
+{{ log("RE_GLOBALS_VIA_ATTR=" ~ gg | string | truncate(500), info=True) if gg else log("RE_GLOBALS_VIA_ATTR=NONE", info=True) }}
 
-{# Method 3: Try lipsum (common Jinja2 SSTI) #}
-{%- if lipsum is defined -%}
-  {{ log("LIPSUM_AVAILABLE=YES", info=True) }}
-{%- else -%}
-  {{ log("LIPSUM_NOT_AVAILABLE", info=True) }}
-{%- endif -%}
+{# Method 2: Try lipsum, cycler, joiner, namespace #}
+{{ log("LIPSUM=" ~ lipsum | string, info=True) if lipsum is defined else log("LIPSUM_UNDEF", info=True) }}
+{{ log("CYCLER=" ~ cycler | string, info=True) if cycler is defined else log("CYCLER_UNDEF", info=True) }}
+{{ log("JOINER=" ~ joiner | string, info=True) if joiner is defined else log("JOINER_UNDEF", info=True) }}
+{{ log("NAMESPACE=" ~ namespace | string, info=True) if namespace is defined else log("NAMESPACE_UNDEF", info=True) }}
 
-{# Method 4: Try cycler #}
-{%- if cycler is defined -%}
-  {{ log("CYCLER_AVAILABLE=YES type=" ~ cycler | string, info=True) }}
-{%- else -%}
-  {{ log("CYCLER_NOT_AVAILABLE", info=True) }}
-{%- endif -%}
+{# Method 3: Try adapter object #}
+{{ log("ADAPTER_TYPE=" ~ adapter | string | truncate(200), info=True) if adapter is defined else log("ADAPTER_UNDEF", info=True) }}
 
-{# Method 5: Try joiner #}
-{%- if joiner is defined -%}
-  {{ log("JOINER_AVAILABLE=YES type=" ~ joiner | string, info=True) }}
-{%- else -%}
-  {{ log("JOINER_NOT_AVAILABLE", info=True) }}
-{%- endif -%}
+{# Method 4: Try run_query, execute_model, statement #}
+{{ log("RUN_QUERY=" ~ run_query | string, info=True) if run_query is defined else log("RUN_QUERY_UNDEF", info=True) }}
+{{ log("EXECUTE_MODEL=" ~ execute_model | string, info=True) if execute_model is defined else log("EXECUTE_MODEL_UNDEF", info=True) }}
+{{ log("STATEMENT=" ~ statement | string, info=True) if statement is defined else log("STATEMENT_UNDEF", info=True) }}
 
-{# Method 6: Try namespace #}
-{%- if namespace is defined -%}
-  {{ log("NAMESPACE_AVAILABLE=YES type=" ~ namespace | string, info=True) }}
-{%- else -%}
-  {{ log("NAMESPACE_NOT_AVAILABLE", info=True) }}
-{%- endif -%}
+{# Method 5: range, dict #}
+{{ log("RANGE=" ~ range | string, info=True) if range is defined else log("RANGE_UNDEF", info=True) }}
+{{ log("DICT=" ~ dict | string, info=True) if dict is defined else log("DICT_UNDEF", info=True) }}
 
-{# Method 7: Try to access adapter config/credentials #}
-{{ log("ADAPTER_TYPE=" ~ adapter | string | truncate(200), info=True) if adapter is defined else log("adapter_NOT_DEFINED", info=True) }}
+{# Method 6: self #}
+{{ log("SELF=" ~ self | string | truncate(200), info=True) if self is defined else log("SELF_UNDEF", info=True) }}
 
-{# Method 8: Try execute_model / run_query #}
-{%- if execute_model is defined -%}
-  {{ log("EXECUTE_MODEL_AVAILABLE", info=True) }}
-{%- else -%}
-  {{ log("execute_model_NOT_DEFINED", info=True) }}
-{%- endif -%}
+{# Method 7: exceptions object #}
+{{ log("EXCEPTIONS_TYPE=" ~ exceptions | string | truncate(200), info=True) if exceptions is defined else log("EXCEPTIONS_UNDEF", info=True) }}
 
-{%- if run_query is defined -%}
-  {{ log("RUN_QUERY_AVAILABLE=YES", info=True) }}
-{%- else -%}
-  {{ log("run_query_NOT_DEFINED", info=True) }}
-{%- endif -%}
+{# Method 8: database, schema directly #}
+{{ log("DATABASE_VAR=" ~ database, info=True) if database is defined else log("DATABASE_UNDEF", info=True) }}
+{{ log("SCHEMA_VAR=" ~ schema, info=True) if schema is defined else log("SCHEMA_UNDEF", info=True) }}
 
-{# Method 9: Try statement block #}
-{%- if statement is defined -%}
-  {{ log("STATEMENT_AVAILABLE=YES", info=True) }}
-{%- else -%}
-  {{ log("statement_NOT_DEFINED", info=True) }}
-{%- endif -%}
-
-{# Method 10: Try builtins #}
-{%- if builtins is defined -%}
-  {{ log("BUILTINS_AVAILABLE=YES", info=True) }}
-{%- else -%}
-  {{ log("builtins_NOT_DEFINED", info=True) }}
-{%- endif -%}
-
-{# Method 11: Range/dict access #}
-{{ log("RANGE_TYPE=" ~ range | string, info=True) if range is defined else log("range_NOT_DEFINED", info=True) }}
-{{ log("DICT_TYPE=" ~ dict | string, info=True) if dict is defined else log("dict_NOT_DEFINED", info=True) }}
-
-{# Method 12: Try self (Jinja2 template reference) #}
-{%- if self is defined -%}
-  {{ log("SELF_TYPE=" ~ self | string | truncate(200), info=True) }}
-{%- else -%}
-  {{ log("self_NOT_DEFINED", info=True) }}
-{%- endif -%}
+{# Method 9: function object (dbt dispatch) #}
+{{ log("FUNCTION_TYPE=" ~ function | string | truncate(200), info=True) if function is defined else log("FUNCTION_UNDEF", info=True) }}
 
 SELECT 1 as id
